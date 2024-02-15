5e13d8d91b19ea1d7c77587d220743c1546f5505
Hammad ul haq <professorcoding123@gmail.com>
2024-02-15 13:55:34 +0500

    Success alert added

--- src/templates/GlobalTemplate.jsx
+++ src/templates/GlobalTemplate.jsx
@@ -7,12 +7,14 @@ import Navbar from '../components/global/Navbar';
 import AuthenticatoinRequired from '../pages/AuthenticatoinRequired';
 import { useContext, useEffect, useState } from "react";
 import Error from '../components/global/Error';
+import Success from '../components/global/Success';
 
 const GlobalTemplate = ({page}) => {
     return (
         <main className="w-full min-h-screen h-auto flex flex-col justify-start items-start relative" >
             <Navbar />
             <Error />
+            <Success />
             <CartSidebar />
             {page}
             <Footer />
